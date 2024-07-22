878e8a61296a5be2bff41076a618dd8986c1e37a
Cole <colepalffy1@gmail.com>
2024-07-23 09:27:21 +1200

    Added feature 1

--- Greeter.java
+++ Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
 
   public String sayHello() {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
-    return messages.getString("ERROR") + ", " + name;
+    return messages.getString("ERROR") + ", " + name.toUpperCase();
   }
 
   public static void main(String[] args) {
